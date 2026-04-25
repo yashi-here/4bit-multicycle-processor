7e37efd5e3790bc70b6183c0039af9c8b0a31010
yashi-here <yashika6982@gmail.com>
2026-04-25 10:21:14 +0530

    Update pc.v

--- rtl/pc.v
+++ rtl/pc.v
@@ -1,15 +1,14 @@
 module pc (
     input  wire clk,
-    input  wire reset_n,
+    input  wire reset,
     input  wire enable,
     output reg  [3:0] pc_out
 );
 
-always @(posedge clk or negedge reset_n) begin
-    if (!reset_n)
+always @(posedge clk or posedge reset)
+    if (reset)
         pc_out <= 4'b0000;
     else if (enable)
         pc_out <= pc_out + 1;
-end
 
 endmodule
